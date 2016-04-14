b104179c65bff7fd26384483b47fb90724d553ae
smitha110 <smitha110@mailbox.winthrop.edu>
2016-04-14 14:46:38 +0000

    Recreated entities, generated crud, and added 2 articles into "article" database.

--- news/src/NewsBundle/Entity/Article.php
+++ news/src/NewsBundle/Entity/Article.php
@@ -3,17 +3,14 @@
 namespace NewsBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\HttpFoundation\File\File;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
- * Articles
+ * Article
  *
- * @ORM\Table(name="articles")
- * @ORM\Entity(repositoryClass="NewsBundle\Repository\ArticlesRepository")
- * @Vich\Uploadable
+ * @ORM\Table(name="article")
+ * @ORM\Entity(repositoryClass="NewsBundle\Repository\ArticleRepository")
  */
-class Articles
+class Article
 {
     /**
      * @var int
@@ -37,20 +34,6 @@ class Articles
      * @ORM\Column(name="content", type="text")
      */
     private $content;
-    
-    /** 
-     * @Vich\UploadableField(mapping="article_image", fileNameProperty="imageName")
-     * 
-     * @var File
-     */
-    private $imageFile;
-    
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="picture", type="string", length=255)
-     */
-    private $imageName;
 
     /**
      * @var string
@@ -74,7 +57,7 @@ class Articles
      * Set title
      *
      * @param string $title
-     * @return Articles
+     * @return Article
      */
     public function setTitle($title)
     {
@@ -96,8 +79,8 @@ class Articles
     /**
      * Set content
      *
-     * @param string $content
-     * @return Articles
+     * @param text $content
+     * @return Article
      */
     public function setContent($content)
     {
@@ -109,7 +92,7 @@ class Articles
     /**
      * Get content
      *
-     * @return string 
+     * @return text 
      */
     public function getContent()
     {
@@ -117,57 +100,10 @@ class Articles
     }
 
     /**
-     * 
-     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
-     *
-     * @return Product
-     */
-    public function setImageFile(File $image = null)
-    {
-        $this->imageFile = $image;
-
-        if ($image) {
-            // It is required that at least one field changes if you are using doctrine
-            // otherwise the event listeners won't be called and the file is lost
-            $this->updatedAt = new \DateTime('now');
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return File
-     */
-    public function getImageFile()
-    {
-        return $this->imageFile;
-    }
-
-    /**
-     * @param string $imageName
-     *
-     * @return Articles
-     */
-    public function setImageName($imageName)
-    {
-        $this->imageName = $imageName;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getImageName()
-    {
-        return $this->imageName;
-    }
-
-    /**
      * Set category
      *
      * @param string $category
-     * @return Articles
+     * @return Article
      */
     public function setCategory($category)
     {
