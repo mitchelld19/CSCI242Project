f99a3653b6176082f28c3f0d4db8716dd32328ae
mitchelld19 <mitchelld19@winthrop.edu>
2016-04-18 02:20:17 +0000

    Removed accounts table and recreated it under name of user, setup FOS User Bundle, and overrode FOS User Bundle template. Currently recieving error when inserting a new user into user table using FOS.

---
+++ news/src/NewsBundle/Entity/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace NewsBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
